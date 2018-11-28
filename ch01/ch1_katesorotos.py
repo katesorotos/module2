19e7476fca3907e0703843add19d93f764e4ba60
Kate Sorotos <kate_sorotos@live.co.uk>
2018-11-28 16:15:51 +0000

    inital commit

---
+++ ch01/ch1_katesorotos.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+A = 2 + 3
+B = 2.9 + 3.2
+C = (3.2/2.0) + 7
+D = 2**3
+E = 4 **(1/2)
+F = str(4) + 'a'
+print(A)
+print(B)
+
+2+3
+2.9+3.2
+(3.2/2.0) + 7
+2**3
+4**(1/2)
+str(6) + 'a'
+
