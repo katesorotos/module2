f3dbe92ef17625c6dda6d6d029fe6c06183231f4
Kate Sorotos <kate_sorotos@live.co.uk>
2018-12-03 15:12:48 +0000

    exercises 10-26 of LPTHW

--- ch02/ex1.py
+++ ch02/ex1.py
@@ -160,3 +160,215 @@ print(formatter % (
     "But it didn't sing.",
     "So I said goodnight."
 ))
+
+#exercise 9
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double- quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6. """)
+
+#exercise 10
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+ 
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
+#
+#while True:
+# for i in ["/","- ","|","\\","|"]:
+#     print("%s\r" % i,)
+
+#exercise 11
+#print("How old are you?",)
+#age = input()
+#print("How tall are you?",)
+#height = input()
+#print("How much do you weigh?",)
+#weight = input()
+#
+#print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
+#
+##exercise 12
+#
+#age = input("How old are you? ")
+#height = input("How tall are you? ")
+#weight = input("How much do you weigh? ")
+#
+#print ("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
+
+#exercise 13
+#exercise 14
+#exercise 15
+#exercise 16
+#exercise 17
+
+#exercise 18
+def print_two(*args):
+    arg1, arg2 = args
+    print("arg1: %r, arg2: %r" % (arg1, arg2))
+
+def print_two_again(arg1, arg2):
+    print("arg1: %r, arg2: %r" % (arg1, arg2))
+
+def print_one(arg1):
+    print("arg1: %r" % arg1)
+
+def print_none():
+    print("I got nothin'.")
+
+print_two("Kate","Sorotos")
+print_two_again("Kate","Sorotos")
+print_one("First!")
+print_none()
+
+#exercise 19
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print("You have %d cheeses!" % cheese_count)
+    print("You have %d boxes of crackers!" % boxes_of_crackers)
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("We can even do math inside too:") 
+cheese_and_crackers(10 + 20, 5 + 6)
+      
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+#exercise 20
+
+#exercise 21
+
+def add(a, b):
+    print("ADDING %d + %d" % (a, b))
+    return a + b
+
+def subtract(a, b):
+    print("SUBTRACTING %d - %d" % (a, b))
+    return a -b
+
+def multiply(a, b):
+    print("MULTIPLYING %d * %d" % (a, b))
+    return a * b
+
+def divide(a, b):
+    print("DIVIDING %d / %d" % (a, b))
+    return a / b
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print("Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq))
+
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
+
+#exercise 22
+#exercise 23
+
+#exercise 24
+print("Let's practice everything.")
+print ('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("- - - - - - - - - - - - - - ")
+print(poem)
+print("- - - - - - - - - - - - - - ")
+
+
+five = 10 - 2 + 3 - 6
+print("This should be five: %s" % five)
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+print("With a starting point of: %d" % start_point)
+print("We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates))
+
+start_point = start_point / 10
+
+print("We can also do that this way:")
+print("We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point))
+
+#exercise 25
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print(word)
+
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(- 1)
+    print(word)
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence): 
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
